1fc702a8351c42ab32a7dfb423d3230bc75ebb31
mattiamurgia <mattiamurgiacontatto@gmail.com>
2024-06-25 16:25:03 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,25 @@
+public class App {
+
+    /**
+     * TODO:
+     * 
+     * completare la classe DataHelper
+     * 
+     * aggiungere i seguenti metodi per testare la classe DataHelper:
+     * - testRandomInteger : void
+     * testare le 3 implementazioni del metodo getRandomInteger
+     * 
+     * - testRandomUniqueInteger : void
+     * testare le 3 implementazioni del metodo getRandomUniqueInteger
+     * 
+     * - testFrequencyMap : void
+     * testare il metodo getFrequencyMap;
+     * sfruttare i metodi precedentemente svolti e utilizzarli come
+     * dati in ingresso per il metodo getFrequencyMap
+     * 
+     */
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/DataHelper.java
@@ -0,0 +1,50 @@
+import java.util.List;
+
+public class DataHelper {
+
+    // TODO: completare i metodi seguenti
+
+    // restituisce una lista di numeri interi casuali compresi tra 0 e 10
+    public static List<Integer> getRandomInteger(int size) {
+
+        return null;
+    }
+
+    // restituisce una lista di numeri interi casuali compresi tra 0 e max
+    public static List<Integer> getRandomInteger(int size, int max) {
+
+        return null;
+    }
+
+    // restituisce una lista di numeri interi casuali compresi tra min e max
+    public static List<Integer> getRandomInteger(int size, int min, int max) {
+
+        return null;
+    }
+
+    // restituisce una lista di numeri casuali NON DUPLICATI compresi tra 0 e 10
+    // public static ??? getRandomUniqueInteger(int size) {
+    //
+    // return null;
+    // }
+
+    // // restituisce una lista di numeri casuali NON DUPLICATI compresi tra 0 e max
+    // public static ??? getRandomUniqueInteger(int size, int max) {
+    //
+    // return null;
+    // }
+
+    // // restituisce una lista di numeri casuali NON DUPLICATI compresi tra min e
+    // max
+    // public static ??? getRandomUniqueInteger(int size, int min, int max) {
+    //
+    // return null;
+    // }
+
+    // restituisce una mappa di frequenza di numeri interi
+    // public static ??? getFrequencyMap(List<Integer> list) {
+    //
+    // return null;
+    // }
+
+}
