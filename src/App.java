3019e6d0f1d480cf55f6ff4e64fec8ee4b7e7dc9
mattiamurgia <mattiamurgiacontatto@gmail.com>
2024-06-26 09:25:07 +0200

    Completed exercises

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
-public class App {
+import java.util.List;
 
+public class App {
     /**
      * TODO:
      * 
@@ -20,6 +21,37 @@ public class App {
      */
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // testRandomMethod();
+        // testRandomUniqueInteger();
+        testFrequencyMap();
     }
+
+    public static void testRandomMethod() {
+
+        System.out.println("getRandomInteger: " + DataHelper.getRandomInteger(10));
+        System.out.println("getRandomInteger with max: " + DataHelper.getRandomInteger(10, 5));
+        System.out.println("getRandomInteger with min and max: " + DataHelper.getRandomInteger(10, 8, 5));
+    }
+
+    public static void testRandomUniqueInteger() {
+
+        System.out.println("getRandomUniqueInteger: " + DataHelper.getRandomUniqueInteger(10));
+        System.out.println("getRandomUniqueInteger with max: " + DataHelper.getRandomUniqueInteger(10, 5));
+        System.out.println("getRandomUniqueInteger with min and max: " + DataHelper.getRandomUniqueInteger(10, 8, 5));
+    }
+
+    public static void testFrequencyMap() {
+
+        List<Integer> e1 = DataHelper.getRandomInteger(10);
+        List<Integer> e2 = DataHelper.getRandomInteger(10, 5);
+        List<Integer> e3 = DataHelper.getRandomInteger(10, 8, 5);
+
+        // System.out.println(e1);
+        System.out.println("getFrequencyMap: " + DataHelper.getFrequencyMap(e1));
+        // System.out.println(e2);
+        System.out.println("getFrequencyMap: " + DataHelper.getFrequencyMap(e2));
+        // System.out.println(e3);
+        System.out.println("getFrequencyMap: " + DataHelper.getFrequencyMap(e3));
+    };
 }
--- src/DataHelper.java
+++ src/DataHelper.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 
 public class DataHelper {
 
@@ -7,44 +13,70 @@ public class DataHelper {
     // restituisce una lista di numeri interi casuali compresi tra 0 e 10
     public static List<Integer> getRandomInteger(int size) {
 
-        return null;
+        return getRandomInteger(size, 0, 10);
     }
 
     // restituisce una lista di numeri interi casuali compresi tra 0 e max
     public static List<Integer> getRandomInteger(int size, int max) {
 
-        return null;
+        return getRandomInteger(size, 0, max);
     }
 
     // restituisce una lista di numeri interi casuali compresi tra min e max
+    // ?* Main Method
     public static List<Integer> getRandomInteger(int size, int min, int max) {
 
-        return null;
+        List<Integer> list = new ArrayList<>();
+        Random rnd = new Random();
+        if (size <= 0) {
+            System.out.println("Inserisci numero maggiore da zero");
+            return list;
+        } else if (min > max) {
+            System.out.println("Il numero di min deve essere minore o uguale a max");
+            return list;
+        } else {
+            for (int i = 0; i < size; i++) {
+                int rndN = rnd.nextInt((max - min) + 1) + min;
+                list.add(rndN);
+            }
+            return list;
+        }
     }
 
     // restituisce una lista di numeri casuali NON DUPLICATI compresi tra 0 e 10
-    // public static ??? getRandomUniqueInteger(int size) {
-    //
-    // return null;
-    // }
+    public static Set<Integer> getRandomUniqueInteger(int size) {
 
-    // // restituisce una lista di numeri casuali NON DUPLICATI compresi tra 0 e max
-    // public static ??? getRandomUniqueInteger(int size, int max) {
-    //
-    // return null;
-    // }
+        return getRandomUniqueInteger(size, 0, 10);
+    }
 
-    // // restituisce una lista di numeri casuali NON DUPLICATI compresi tra min e
+    // restituisce una lista di numeri casuali NON DUPLICATI compresi tra 0 e max
+    public static Set<Integer> getRandomUniqueInteger(int size, int max) {
+
+        return getRandomUniqueInteger(size, 0, max);
+    }
+
+    // restituisce una lista di numeri casuali NON DUPLICATI compresi tra min e
     // max
-    // public static ??? getRandomUniqueInteger(int size, int min, int max) {
-    //
-    // return null;
-    // }
+    // ?* Main Method
+    public static Set<Integer> getRandomUniqueInteger(int size, int min, int max) {
+
+        List<Integer> rnd = getRandomInteger(size, min, max);
+        Set<Integer> s = new HashSet<>(rnd);
+        // System.out.println(rnd);
+        return s;
+    }
 
     // restituisce una mappa di frequenza di numeri interi
-    // public static ??? getFrequencyMap(List<Integer> list) {
-    //
-    // return null;
-    // }
+    public static Map<Integer, Integer> getFrequencyMap(List<Integer> list) {
+
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (Integer n : list) {
+            Integer count = freq.getOrDefault(n, 0);
+            // System.out.println("count: " + count);
+            freq.put(n, count + 1);
+        }
+
+        return freq;
+    }
 
 }
